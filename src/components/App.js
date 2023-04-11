1e6ce92cdc87825650ddaee88233f9926e740016
Gabrielle Glasgow <g.glasgow91@gmail.com>
2023-04-11 16:53:16 -0400

    completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,26 @@
 import React from "react";
+import TopMenu from './TopMenu'; //username circle
+import SideMenu from './SideMenu';
+import MainContent from "./MainContent";
+
+
+
+
+;
+
 
 function App() {
-  return <div className="app-container">{/* What components go here? */}</div>;
+  return <div className="app-container">
+    < TopMenu />
+    < SideMenu />
+    < MainContent />
+    {/* {
+    
+   
+    
+    
+    * /} */}
+  </div>;
 }
 
 export default App;
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,7 +1,17 @@
 import React from "react";
+import GenrePicker from './GenrePicker';
+import AlbumList from "./AlbumList";
+import AlbumCard from './AlbumCard';
+
 
 function MainContent() {
-  return <main>{/* What components go here? */}</main>;
+  return <main>
+    {/* What components go here? */}
+    < GenrePicker />
+    < AlbumList />
+    < AlbumCard />
+
+  </main>;
 }
 
 export default MainContent;
